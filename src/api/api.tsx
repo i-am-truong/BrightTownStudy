a47b6a78c8580b9d679d5bf640421e6a5fee4b49
Trương Quốc Trường <tqtolympia@gmail.com>
2025-06-13 10:11:26 +0700

    chore: change domain API

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://json-server-vercel-vert-mu.vercel.app/",
+  baseURL: "https://apibts.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
